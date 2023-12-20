09072a2e8a11ea9412654dd393b1e5ca429692e5
latvia <126933766+bachwebsite@users.noreply.github.com>
2023-12-19 20:34:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Get all elements with the class "fade-in"
+var fadeElements = document.querySelectorAll('.fade-in');
+
+// Add scroll event listener
+window.addEventListener('scroll', function () {
+    // Loop through each fade-in element
+    fadeElements.forEach(function (element) {
+        // Calculate the distance from the top of the element to the top of the viewport
+        var distanceFromTop = element.getBoundingClientRect().top;
+
+        // If the element is in the viewport, add the "show" class
+        if (distanceFromTop - window.innerHeight < 0) {
+            element.classList.add('show');
+        }
+    });
+});
+
+// Get the header element
 var header = document.getElementById("main-header");
 
 // Get the offset position of the header
