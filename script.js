1ebd55f182f7e85f8e8e56e04ca998e1a5455230
latvia <126933766+bachwebsite@users.noreply.github.com>
2023-12-21 14:09:36 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.width = sidebar.style.width === '200px' ? '0' : '200px'; // Adjust width as needed
+}
