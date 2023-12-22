afab52a90775d23e5fb5d56f80a13312cd689e49
latvia <126933766+bachwebsite@users.noreply.github.com>
2023-12-21 19:56:57 -0600

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function toggleSidebar() {
-  const sidebar = document.querySelector('.sidebar');
-  sidebar.style.width = sidebar.style.width === '200px' ? '0' : '200px'; // Adjust width as needed
-}
